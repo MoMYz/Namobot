fc7b06b4fb904dde735686b769097cf6e94015d1
MoMYz <102386141+MoMYz@users.noreply.github.com>
2022-04-03 23:22:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,7 @@ client.add_cog(music_cog(client))
 
 @client.event
 async def on_ready():
+    print('Bot is ready')
     await client.change_presence(status=discord.Status.online, activity=discord.Game("Namolnwza007 | !command"))
 
 @client.command()
@@ -44,21 +45,21 @@ async def botdev(context):
     await context.message.channel.send(embed=myEmbed)
 
 @client.command()
-async def namotwitch(context):
+async def twitch(context):
     print('here')
     await context.send("https://www.twitch.tv/momyzch")
     
     await client.process_commands(message)
 
 @client.command()
-async def namoig(context):
+async def ig(context):
     print('here')
     await context.send("https://www.instagram.com/namo_pun/")
     
     await client.process_commands(message)
 
 @client.command()
-async def namoyt(context):
+async def yt(context):
     print('here')
     await context.send("https://www.youtube.com/channel/UCWBSrFL8YQpzqYASxaWxtAQ")
     
@@ -86,7 +87,7 @@ async def ความเหลี่ยมของ(context, p5: discord.Member
 @client.command()
 async def ต่อยกับ(ctx, p1: discord.Member):
     player1 = p1
-    choices = ("คุณต่อย <@" + str(player1.id) + ">ปากแตก \U0001F3C6","คุณโดน <@" + str(player1.id) + ">ต่อยปากแตก \U0001F915", "คุณโดน <@" + str(player1.id) + ">เตะก้านคอ \U0001F915", "คุณเตะก้านคอ <@" + str(player1.id) + ">\U0001F3C6", 'น็อคคู่ \U0001F915')
+    choices = ("คุณต่อย <@" + str(player1.id) + ">ปากแตก แล้วกระทืบซ้ำ \U0001F3C6","คุณโดน <@" + str(player1.id) + ">ซัดหน้า ฟันหลุด 2 ซี่ \U0001F915", "คุณโดน <@" + str(player1.id) + ">เตะก้านคอ \U0001F915", "คุณเตะก้านคอ <@" + str(player1.id) + ">\U0001F3C6", 'น็อคคู่ \U0001F915')
     ans = random.choice(choices)
     await ctx.send(ans)      
 
@@ -152,7 +153,7 @@ async def on_message(message):
     if message.content == 'ตุลย์':
         print(message.channel)
         await message.channel.send('กูว่าแล้ว/กูคิดไว้แล้ว')
-    
+
     if message.content == 'นีล':
         print(message.channel)
         await message.channel.send('ไถนาอยู่กับพี่นนครับ')
@@ -170,5 +171,136 @@ async def on_message(message):
         await message.channel.send(ans6)      
 
     await client.process_commands(message)
+
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+@client.command()
+async def place(ctx, pos: int):
+    global turn
+    global player1
+    global player2
+    global board
+    global count
+    global gameOver
+
+    if not gameOver:
+        mark = ""
+        if turn == ctx.author:
+            if turn == player1:
+                mark = ":regional_indicator_x:"
+            elif turn == player2:
+                mark = ":o2:"
+            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                board[pos - 1] = mark
+                count += 1
+
+                # print the board
+                line = ""
+                for x in range(len(board)):
+                    if x == 2 or x == 5 or x == 8:
+                        line += " " + board[x]
+                        await ctx.send(line)
+                        line = ""
+                    else:
+                        line += " " + board[x]
+
+                checkWinner(winningConditions, mark)
+                print(count)
+                if gameOver == True:
+                    await ctx.send(mark + " wins!")
+                elif count >= 9:
+                    gameOver = True
+                    await ctx.send("It's a tie!")
+
+                # switch turns
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+            else:
+                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+        else:
+            await ctx.send("It is not your turn.")
+    else:
+        await ctx.send("Please start a new game using the !tictactoe command.")
+
+
+def checkWinner(winningConditions, mark):
+    global gameOver
+    for condition in winningConditions:
+        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+            gameOver = True
+
+@tictactoe.error
+async def tictactoe_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention 2 players for this command.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
+
+@place.error
+async def place_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter a position you would like to mark.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to enter an integer.")
 
 client.run(os.environ['token'])
